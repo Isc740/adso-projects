5b8abc5acb4b55dba0d0455e54d35d61f7b2b0c2
Isc740 <isaazcantillo@gmail.com>
2024-10-11 21:03:00 -0500

    add user input manager

--- 04-sudoku/src/app.js
+++ 04-sudoku/src/app.js
@@ -1,5 +1,28 @@
-// const 
+const manageUserInput = () => {
+	let selectedValue = null;
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     k
-// })
+	const selectionButtons = document.querySelectorAll(
+		".selection-container input",
+	);
+
+	for (button of selectionButtons) {
+		button.addEventListener("click", function () {
+			selectedValue = this.value;
+			console.log(selectedValue);
+		});
+	}
+
+	const gridCells = document.querySelectorAll(".grid-cell");
+	for (cell of gridCells) {
+		cell.addEventListener("click", function () {
+            console.log(this.value);
+			if (selectedValue !== null && this.value === "") {
+				this.value = selectedValue;
+			}
+		});
+	}
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	manageUserInput();
+});
