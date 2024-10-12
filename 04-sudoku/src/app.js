95f007f33413ab3a1fc36a267d7ce2d9ba31075b
Isc740 <isaazcantillo@gmail.com>
2024-10-11 21:22:18 -0500

    made some code refactoring

--- 04-sudoku/src/app.js
+++ 04-sudoku/src/app.js
@@ -1,4 +1,4 @@
-const manageUserInput = () => {
+const manageUserInput = (gridCells) => {
 	let selectedValue = null;
 
 	const selectionButtons = document.querySelectorAll(
@@ -8,14 +8,11 @@ const manageUserInput = () => {
 	for (button of selectionButtons) {
 		button.addEventListener("click", function () {
 			selectedValue = this.value;
-			console.log(selectedValue);
 		});
 	}
 
-	const gridCells = document.querySelectorAll(".grid-cell");
 	for (cell of gridCells) {
 		cell.addEventListener("click", function () {
-            console.log(this.value);
 			if (selectedValue !== null && this.value === "") {
 				this.value = selectedValue;
 			}
@@ -23,6 +20,11 @@ const manageUserInput = () => {
 	}
 };
 
+const randomize_grid = () => {
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-	manageUserInput();
+	const gridCells = document.querySelectorAll(".grid-cell");
+
+	manageUserInput(gridCells);
 });
