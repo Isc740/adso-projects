473003b05e47abf7c7105355f3b19d2fc03ab6fc
Isc740 <isaazcantillo@gmail.com>
2024-10-16 18:29:51 -0500

    add basic reset button

--- 01-web-dev/03-sudoku/src/app.js
+++ 01-web-dev/03-sudoku/src/app.js
@@ -1,28 +1,37 @@
+const manageResets = () => {
+    const grids = document.querySelectorAll(".grid");
+    document.getElementById("reset").addEventListener("click", function () {
+        for (const grid of grids) {
+            for (const cell of grid.children) {
+                cell.value = "";
+            }
+        }
+    });
+};
+
 const manageUserInput = () => {
-	let selectedValue = null;
+    let selectedValue = null;
 
-	const selectionButtons = document.querySelectorAll(
-		".selection-container input",
-	);
-	for (button of selectionButtons) {
-		button.addEventListener("click", function () {
-			selectedValue = this.value;
-		});
-	}
+    const selectionButtons = document.querySelectorAll(
+        ".selection-container input",
+    );
+    for (button of selectionButtons) {
+        button.addEventListener("click", function () {
+            selectedValue = this.value;
+        });
+    }
 
-	const gridCells = document.querySelectorAll(".grid-cell");
-	for (cell of gridCells) {
-		cell.addEventListener("click", function () {
-			if (selectedValue !== null && this.value === "") {
-				this.value = selectedValue;
-			}
-		});
-	}
+    const gridCells = document.querySelectorAll(".grid-cell");
+    for (cell of gridCells) {
+        cell.addEventListener("click", function () {
+            if (selectedValue !== null && this.value === "") {
+                this.value = selectedValue;
+            }
+        });
+    }
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-	const grids = document.querySelectorAll(".grid");
-
-	manageUserInput();
-	randomizeGrids(grids);
+    manageUserInput();
+    manageResets();
 });
