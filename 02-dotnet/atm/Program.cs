12205f383055c8bc1e3fa2405764e4d367a494ca
Isc740 <isaazcantillo@gmail.com>
2024-10-17 17:23:45 -0500

    initial dotnet commit

---
+++ 02-dotnet/atm/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*using System;*/
+
+namespace atm
+{
+    class Atm
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
