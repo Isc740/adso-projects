e3e219d83fe90f29bb90e16637c7c905d2b76951
Isc740 <isaazcantillo@gmail.com>
2024-10-16 21:34:23 -0500

    add functions for basic sudoku

--- 01-web-dev/03-sudoku/src/app.js
+++ 01-web-dev/03-sudoku/src/app.js
@@ -7,6 +7,60 @@ const manageResets = () => {
             cell.value = "";
             cell.style.color = "#000";
         });
+        generateRandomSudoku();
     });
     reset = true;
 };
+
+const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
+
+const generateSudokuTemplate = () => {
+    const template = Array(81).fill(0);
+    const baseNumbers = shuffleArray([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 3; j++) {
+            const pos = Math.floor(Math.random() * 9);
+            const index = (Math.floor(i / 3) * 27) + ((i % 3) * 3) +
+                (Math.floor(pos / 3) * 9) + (pos % 3);
+            template[index] = baseNumbers[j];
+        }
+    }
+    return template;
+};
+
+const generateRandomSudoku = () => {
+    const gridCells = document.querySelectorAll(".grid-cell");
+    const template = generateSudokuTemplate();
+
+    template.forEach((val, index) => {
+        gridCells[index].value = val ? val : ""; // 0 means empty
+        gridCells[index].style.color = "#000";
+    });
+};
+
+const manageUserInput = () => {
+    let selectedValue = null;
+
+    document.querySelectorAll(".selection-container input").forEach(
+        (button) => {
+            button.addEventListener(
+                "click",
+                () => (selectedValue = button.value),
+            );
+        },
+    );
+
+    document.querySelectorAll(".grid-cell").forEach((cell) => {
+        cell.addEventListener("click", () => {
+            if (selectedValue && !cell.value) {
+                cell.value = selectedValue;
+            }
+        });
+    });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    manageUserInput();
+    manageResets();
+});
